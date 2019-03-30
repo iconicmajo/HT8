d38053c79820d345c4c74cbc039efe77caab6931
iconicmajo <42973036+iconicmajo@users.noreply.github.com>
2019-03-29 19:02:54 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/**
+ * Maria Jose Castro Lemus 
+ * 181202
+ * Estructura de datos
+ * Lab 8
+ */
+
+/**
+ * @author Majo!
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//PriorityQueue<Comparable<E>> prority = new PriorityQueue<Comparable<E>>(); 
+		System.out.println("hello word");
+	}
+
+}
---
+++ Paciente.java
@@ -0,0 +1,72 @@
+/**
+ * Maria Jose Castro Lemus 
+ * 181202
+ * Estructura de datos
+ * Lab 8
+ */
+
+/**
+ * @author Majo!
+ *
+ */
+public class Paciente<E extends Comparable<E>> {
+	
+	private String name;
+	private String sick;
+	private String code;
+	/**
+	 * @param name
+	 * @param sick
+	 * @param code
+	 */
+	public Paciente(String name, String sick, String code) {
+		this.name = name;
+		this.sick = sick;
+		this.code = code;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the sick
+	 */
+	public String getSick() {
+		return sick;
+	}
+	/**
+	 * @param sick the sick to set
+	 */
+	public void setSick(String sick) {
+		this.sick = sick;
+	}
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Paciente [name=" + name + ", sick=" + sick + ", code=" + code + "]";
+	}
+	
+}
+
